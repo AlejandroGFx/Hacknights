0b3c14ffb36e2adb179d4340b7ff184474d44007
Alejandro <alejandroogutierrez06@gmail.com>
2026-02-28 00:06:48 +0100

    Initial commit

---
+++ src/main/java/org/example/hacknights/HacknightsApplication.java
@@ -0,0 +1,13 @@
+package org.example.hacknights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HacknightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HacknightsApplication.class, args);
+	}
+
+}
